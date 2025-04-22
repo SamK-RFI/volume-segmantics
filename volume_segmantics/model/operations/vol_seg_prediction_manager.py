46e1a200d7135fdc0eab4fd57d4d588f644fb57e
Neville B.-y. Yee <neville.yee@rfi.ac.uk>
2025-04-22 13:58:26 +0000

    Optimised prediction sequence and updated default parameter file to lift the restriction on mutual exclusivity between output_probs and output_entropy.

--- volume_segmantics/model/operations/vol_seg_prediction_manager.py
+++ volume_segmantics/model/operations/vol_seg_prediction_manager.py
@@ -64,6 +64,8 @@ class VolSeg2DPredictionManager(BaseDataManager):
         """
         probs = None
         logits = None
+        entropy = None
+
         one_hot = self.settings.one_hot
         output_probs = self.settings.output_probs
         output_entropy = self.settings.output_entropy
@@ -84,24 +86,32 @@ class VolSeg2DPredictionManager(BaseDataManager):
         if quality == utils.Quality.MEDIUM:
             if one_hot:
                 prediction = self.predictor._predict_3_ways_one_hot(self.data_vol)
-            elif output_probs:
+            elif output_probs and not output_entropy:
                 prediction, probs = self.predictor._predict_3_ways_max_probs(
                     self.data_vol
                 )
-            elif output_entropy:
-                prediction, entropy = self.predictor._prediction_estimate_entropy(
+            elif output_entropy and not output_probs:
+                prediction, _, entropy = self.predictor._prediction_estimate_entropy(
+                    self.data_vol
+                )
+            elif output_entropy and output_probs:
+                prediction, probs, entropy = self.predictor._prediction_estimate_entropy(
                     self.data_vol
                 )
 
         if quality == utils.Quality.HIGH:
             if one_hot:
                 prediction = self.predictor._predict_12_ways_one_hot(self.data_vol)
-            elif output_probs:
+            elif output_probs and not output_entropy:
                 prediction, probs = self.predictor._predict_12_ways_max_probs(
                     self.data_vol
                 )
-            elif output_entropy:
-                prediction, entropy = self.predictor._prediction_estimate_entropy(
+            elif output_entropy and not output_probs:
+                prediction, _, entropy = self.predictor._prediction_estimate_entropy(
+                    self.data_vol
+                )
+            elif output_entropy and output_probs:
+                prediction, probs, entropy = self.predictor._prediction_estimate_entropy(
                     self.data_vol
                 )
 
